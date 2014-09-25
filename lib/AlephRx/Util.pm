41a63b094ccfb3cfabb721e81627ddf5a2400495
Peter Eichman <peichman@umd.edu>
2014-09-25 11:04:11 -0400

    Added perldoc to the AlephRx::Util module.

--- lib/AlephRx/Util.pm
+++ lib/AlephRx/Util.pm
@@ -1,8 +1,48 @@
 package AlephRx::Util;
 
+=head1 NAME
+
+AlephRx::Util - AlephRx application configuration and utilities.
+
+=head1 DESCRIPTION
+
+This module gathers together application-level configuration information used by
+the various scripts of the AlephRx application.
+
+=head2 $AlephRx::Util::FROM
+
+The email address used in the C<From:> header of all notification emails. Per
+issue LIBILS-37, this cannot be a reflector address that might show up in the
+C<To:> header, since the UMD email system will likely reject that email.
+
+=cut
 our $FROM = 'AlephRx <alephrx@umd.edu>';
+
+=head2 $AlepRx::Util::REPLY_TO
+
+The email address used in the C<Reply-To:> header of all notification emails.
+While users are instructed not to reply directly to the notification emails,
+this is put in place so that just in case they do, the replies will go to a
+mailbox or list that is being monitored.
+
+=cut
 our $REPLY_TO = 'usmaialeph@umd.edu';
 
+=head2 @AlephRx::Util::FUNCTIONAL_AREAS
+
+List of all functional areas used in the application. Each item is a hashref
+with the keys C<name>, C<recipient>, and C<slug>.
+
+The C<name> is both the display name for the area in the UI as well as the value
+that is stored in the database.
+
+The C<recipient> is the email address or addresses for the default recipient of
+notification emails for reports with a given functional area.
+
+The C<slug> is a short abbreviation for the functional area that is used int he
+email subject line.
+
+=cut
 our @FUNCTIONAL_AREAS = (
     {
         name => 'Password reset',
@@ -71,6 +111,15 @@ our @FUNCTIONAL_AREAS = (
     },
 );
 
+=head2 %AlephRx::Util::RECIPIENT_FOR
+
+Mapping of functional area C<name> to C<recipient>.
+
+=head2 %AlephRx::Util::SLUG_FOR
+
+Mapping of functional area C<name> to C<slug>.
+
+=cut
 # mappings for quick recipient and slug lookups, given the functional area name
 our %RECIPIENT_FOR = map { $_->{name} => $_->{recipient} } @FUNCTIONAL_AREAS;
 our %SLUG_FOR      = map { $_->{name} => $_->{slug}      } @FUNCTIONAL_AREAS;
